d006343ca89a77c33a8fdb9d8913469745881b86
HeChu <2296282722@qq.com>
2025-03-13 00:18:31 +0800

    Initial commit

---
+++ HiedaCamellia.ProjectBase/src/Package.cs
@@ -0,0 +1,11 @@
+namespace HiedaCamellia.ProjectBase;
+
+/// <summary>
+/// Package class.
+/// </summary>
+public class Package {
+  /// <summary>
+  /// Method.
+  /// </summary>
+  public string Method() => "Hello, world!";
+}
